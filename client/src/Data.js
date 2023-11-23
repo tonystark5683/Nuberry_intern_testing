bff5acef955d100efa2769e9fda3b29d5d2b4809
Debdutta <debduttac70@gmail.com>
2023-11-23 12:06:56 +0530

    graph implemeted

---
+++ client/src/Data.js
@@ -0,0 +1,173 @@
+export const StoreData = [
+    {
+      id: 1,
+      name: "Chrompet",
+      Sales: 80000,
+      departments:[
+        {
+          label:"clothing",
+          sales:300,
+          subCategory:{
+            cat1:150,
+            cat2:150
+          }
+        },
+        {
+          label:"Accessories",
+          sales:500,
+          subCategory:{
+            cat1:300,
+            cat2:200
+          }
+        },
+        {label:"Baby care",
+        sales:800,
+        subCategory:{
+          cat1:300,
+          cat2:200,
+          cat3:200
+  
+        }
+        }
+      ]
+        
+      
+    },
+    {
+      id: 2,
+      name: 'Titan',
+      Sales: 45677,
+      departments:[
+        {
+          label:"clothing",
+          sales:900,
+          subCategory:{
+            cat1:450,
+            cat2:150,
+            cat3:300
+          }
+        },
+        {
+          label:"Accessories",
+          sales:600,
+          subCategory:{
+            cat1:300,
+            cat2:200,
+            cat3:100
+          }
+        },
+        {label:"Baby care",
+        sales:800,
+        subCategory:{
+          cat1:300,
+          cat2:200,
+          cat3:300
+  
+        }
+        }
+      ]
+        
+    },
+    {
+      id: 3,
+      name: 'Ajanta',
+      Sales: 78888,
+      departments:[
+        {
+          label:"clothing",
+          sales:300,
+          subCategory:{
+            cat1:150,
+            cat2:150,
+            cat3:200
+          }
+        },
+        {
+          label:"Accessories",
+          sales:500,
+          subCategory:{
+            cat1:300,
+            cat2:200
+          }
+        },
+        {label:"Baby care",
+        sales:700,
+        subCategory:{
+          cat1:300,
+          cat2:200,
+          cat3:200
+  
+        }
+        }
+      ]
+        
+    },
+    {
+      id: 4,
+      name: 'Virat_clothing',
+      Sales: 90000,
+      departments:[
+        {
+          label:"clothing",
+          sales:300,
+          subCategory:{
+            cat1:150,
+            cat2:150
+          }
+        },
+        {
+          label:"Accessories",
+          sales:500,
+          subCategory:{
+            cat1:300,
+            cat2:200
+          }
+        },
+        {label:"Baby care",
+        sales:700,
+        subCategory:{
+          cat1:300,
+          cat2:200,
+          cat3:200
+  
+        }
+        }
+      ]
+        
+    },
+    {
+      id: 5,
+      name: 'SRK_clothing',
+      Sales: 4300,
+      departments:[
+        {
+          label:"clothing",
+          sales:300,
+          subCategory:{
+            cat1:150,
+            cat2:150
+          }
+        },
+        {
+          label:"Accessories",
+          sales:500,
+          subCategory:{
+            cat1:300,
+            cat2:200
+          }
+        },
+        {label:"Baby care",
+        sales:700,
+        subCategory:{
+          cat1:300,
+          cat2:200,
+          cat3:200
+  
+        }
+        }
+      ]
+        
+    },
+  ];
+  
+  
--- client/src/components/ForecastGraph.jsx
+++ client/src/components/ForecastGraph.jsx
@@ -1,323 +1,138 @@
-import React, { useEffect } from 'react';
-import Chart from 'chart.js/auto';
+import { useRef, useState } from "react";
+import Chart from "chart.js/auto";
+import { Bar, getElementAtEvent } from "react-chartjs-2";
+import { StoreData } from "../Data";
 
 const ForecastGraph = () => {
-  useEffect(() => {
-    const coordinates = {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-    };
-
-    const storeData  = [
-      { store: 'Chrompet', color: 'rgba(75, 192, 192, 1)', users: 150, marketshare: 140, departments: [
-        { department: 'Clothing', users: 30, categories: [
-          { category: 'cat1', users: 23 },
-          { category: 'cat2', users: 46 },
-          { category: 'cat3', users: 12 },
-          { category: 'cat4', users: 30 },
-        ]},
-        { department: 'Accessories', users: 70, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Care', users: 40, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Footwear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Gear', users: 50, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Bedding & Furniture', users: 50, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Games & Toys', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-      ]},
-      { store: 'Adyar', color: 'rgba(75, 192, 192, 1)', users: 50, marketshare: 80, departments: [
-        { department: 'Clothing', users: 30, categories: [
-          { category: 'Men', users: 5 },
-          { category: 'Women', users: 5 },
-        ]},
-        { department: 'Accessories', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Care', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Footwear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Gear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Bedding & Furniture', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Games & Toys', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-      ]},
-      { store: 'Shanthi Colony', color: 'rgba(75, 192, 192, 1)', users: 250, marketshare: 40, departments: [
-        { department: 'Clothing', users: 43, categories: [
-          { category: 'Men', users: 5 },
-          { category: 'Women', users: 5 },
-        ]},
-        { department: 'Accessories', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Care', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Footwear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Gear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Bedding & Furniture', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Games & Toys', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-      ]},
-      { store: 'Velachery', color: 'rgba(75, 192, 192, 1)', users: 250, marketshare:27, departments: [
-        { department: 'Clothing', users: 56, categories: [
-          { category: 'Men', users: 5 },
-          { category: 'Women', users: 5 },
-        ]},
-        { department: 'Accessories', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Care', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Footwear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Gear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Bedding & Furniture', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Games & Toys', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-      ]},
-      { store: 'Sarath Mall', color: 'rgba(75, 192, 192, 1)', users: 250, marketshare: 87, departments: [
-        { department: 'Clothing', users: 42, categories: [
-          { category: 'Men', users: 5 },
-          { category: 'Women', users: 5 },
-        ]},
-        { department: 'Accessories', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Care', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Footwear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Baby Gear', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Bedding & Furniture', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-        { department: 'Games & Toys', users: 20, categories: [
-          { category: 'Jewelry', users: 10 },
-          { category: 'Handbags', users: 10 },
-        ]},
-      ]}, 
-
-    ];
-
-    const data = {
-      datasets: [
-        {
-          label: 'Stores',
-          data: storeData,
-          backgroundColor: 'rgba(75, 192, 192, 1)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
-
-    const resetButton = {
-      id: 'resetButton',
-      beforeDraw(chart, args, options) {
-        const { ctx, chartArea: { top, bottom, left, right, width, height } } = chart;
-        ctx.save();
-
-        const text = 'Back';
-        const thickBorder = 3;
-        const textWidth = ctx.measureText(text).width;
-
-        ctx.fillStyle = 'rgba(255, 26, 104, 0.2)';
-        ctx.fillRect(right - (textWidth + 1 + 10), 5, textWidth + 10, 20);
-
-        ctx.fillStyle = '#666';
-        ctx.font = '12px Arial';
-        ctx.fillText(text, right - (textWidth + 1 + 5), 15);
-
-        ctx.lineWidth = thickBorder + 'px';
-        ctx.strokeStyle = 'rgba(255, 26, 104, 1)';
-        ctx.strokeRect(right - (textWidth + 1 + 10), 5, textWidth + 10, 20);
-
-        coordinates.top = 5;
-        coordinates.bottom = 25;
-        coordinates.left = right - (textWidth + 1 + 10);
-        coordinates.right = right;
-
-        ctx.restore();
+  const chartref = useRef(null);
+  const [index, setIndex] = useState([]);
+  const [userData, setUserData] = useState({
+    labels: StoreData.map((data) => data.name),
+    datasets: [
+      {
+        label: "Total_Sales",
+        data: StoreData.map((data) => data.Sales),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
       },
-    };
+    ],
+  });
+  const handleStateChange = (currindex) => {
+    setUserData(
+      index.length === 0
+        ? {
+            labels: StoreData[currindex].departments.map(
+              (departmentName) => departmentName.label
+            ),
+            datasets: [
+              {
+                label: "Department_Wise_sales",
+                data: StoreData[currindex].departments.map(
+                  (departmentName) => departmentName.sales
+                ),
 
-    const config = {
-      type: 'bar',
-      data,
-      options: {
-        plugins: {
-          tooltip: {
-            yAlign: 'bottom',
-          },
-        },
-        onHover: (event, chartElement) => {
-          const level = myChart.config.data.datasets[0].label.toLowerCase();
-          event.native.target.style.cursor = (level === 'stores' && chartElement[0]) ? 'pointer' : 'default';
-        },
-        parsing: {
-          xAxisKey: 'store',
-          yAxisKey: 'marketshare',
-        },
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-        plugins: [resetButton],
-      },
-    };
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+              },
+            ],
+          }
+        : {
+            labels: Object.keys(
+              StoreData[index[index.length - 1]].departments[currindex].subCategory
+            ),
+            datasets: [
+              {
+                label: "Department_Wise_sales",
+                data: Object.values(
+                  StoreData[index[index.length - 1]].departments[currindex].subCategory
+                ),
+              },
+            ],
+          }
+    );
+  };
 
-    const ctx = document.getElementById('myChart');
+  const handleBackChange = ( currindex) => {
+    setUserData(
+      index.length === 2
+        ? {
+            labels: StoreData[currindex].departments.map(
+              (departmentName) => departmentName.label
+            ),
+            datasets: [
+              {
+                label: "Department_Wise_sales",
+                data: StoreData[currindex].departments.map(
+                  (departmentName) => departmentName.sales
+                ),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+              },
+            ],
+          }
+        : {
+            labels: StoreData.map((data) => data.name),
+            datasets: [
+              {
+                label: "Total_Sales",
+                data: StoreData.map((data) => data.Sales),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          }
+    );
+  };
 
-    const myChart = new Chart(ctx, config);
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+  const handleClick = (e) => {
+    console.log("event,", getElementAtEvent(chartref.current, e));
+    const currindex = getElementAtEvent(chartref.current, e)[0].index;
 
-    function changeChart(level,index) {
-      const currentData = level === 'store' ? storeData : (level === 'department' ? storeData[index].departments : storeData[index].departments[index].categories);
-      // const vColor = [];
-      const vColor = 'rgba(75, 192, 192, 1)';
-      const vUsers = [];
-      const vLabels = currentData.map((item) => {
-        // vColor.push(item.color);
-        vUsers.push(item.users || 0);
-        return item.category || item.department || item.store;
-      });
-      myChart.config.data.datasets[0].data = vUsers;
-      myChart.config.data.labels = vLabels;
-      myChart.config.data.datasets[0].backgroundColor = vColor;
-      myChart.config.data.datasets[0].borderColor = vColor;
-      myChart.config.data.datasets[0].label = level === 'store' ? 'Stores' : (level === 'department' ? 'departments' : 'categories');
-      myChart.update();
+    handleStateChange(currindex);
+    if (index.length< 2) {
+      setIndex((prevIndex) => [...prevIndex, currindex]);
     }
-
-    function clickHandler(click) {
-      // if (myChart.config.data.datasets[0].label === 'Forcast Data') {
-      //   const bar = myChart.getElementsAtEventForMode(click, 'nearest', { intersect: true }, true);
-      //   console.log(bar);
-      //   if (bar.length) {
-      //     console.log(bar[0].index);
-      //     changeChart(bar[0].index);
-      //   }
-      // }
-      const level = myChart.config.data.datasets[0].label.toLowerCase();
-  if (level === 'stores' && myChart.config.data.labels[0] === 'Chrompet') {
-    // Simulating drill-down from Store A to its departments
-    changeChart('department', 0);
-  } else if (level === 'departments' && myChart.config.data.labels[0] === 'Clothing') {
-    // Simulating drill-down from Department 1 to its sub-departments
-    changeChart('category', 0);
-  }
-    }
-
-    function resetChart() {
-      console.log('clicked reset Button!');
-    }
-
-    function mousemoveHandler(canvas, mousemove) {
-      const x = mousemove.offsetX;
-      const y = mousemove.offsetY;
-
-      if (x > coordinates.left && x < coordinates.right && y > coordinates.top && y < coordinates.bottom) {
-        canvas.style.cursor = 'pointer';
-      } else {
-        canvas.style.cursor = 'default';
-      }
-    }
-
-    function clickButtonHandler(canvas, click) {
-      const x = click.offsetX;
-      const y = click.offsetY;
-
-      if (x > coordinates.left && x < coordinates.right && y > coordinates.top && y < coordinates.bottom) {
-        resetChart();
-      }
-    }
-
-    ctx.onclick = clickHandler;
-    ctx.addEventListener('mousemove', (e) => {
-      mousemoveHandler(ctx, e);
-    });
-    ctx.addEventListener('click', (e) => {
-      clickButtonHandler(ctx, e);
-    });
-
-    // Cleanup function to destroy the chart on component unmount
-    return () => {
-      myChart.destroy();
-    };
-  }, []); // Empty dependency array to run the effect only once on mount
+  };
+  console.log("count" ,index);
+  const handleBackClick = (e) => {
+    const indexVal=index.length-1
+    handleBackChange(index[indexVal]);
+    setIndex((prevIndex) => prevIndex.splice(indexVal, 1));
+  };
 
   return (
-    <div style={{ width: '60%', height: '80vh', display:'flex',flexDirection:'column', justifyContent:'center',alignItems:'center', margin:'auto'}}>      <canvas id="myChart" ></canvas>
+    <div className="App">
+      <div style={{ width: 700 }}>
+        <Bar ref={chartref} data={userData} onClick={handleClick} />
+      </div>
+      <button onClick={handleBackClick}>Check the previous graph</button>
     </div>
   );
 };
 
 export default ForecastGraph;
-
